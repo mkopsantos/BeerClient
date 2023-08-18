87d393377bcab83bc713cae7ac56bc2987e45243
Markus Kopinits <markus.kopinits@gft.com>
2023-08-18 16:36:46 +0200

    spotlless apply

--- src/main/java/com/mkop/beerclient/config/WebClientProperties.java
+++ src/main/java/com/mkop/beerclient/config/WebClientProperties.java
@@ -1,10 +1,5 @@
 package com.mkop.beerclient.config;
 
-import org.springframework.web.util.UriBuilder;
-
-import java.net.URI;
-import java.util.function.Function;
-
 public class WebClientProperties {
 
     public static final String BASE_URL = "https://api.springframework.guru";
